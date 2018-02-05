3ca213060a78c9cbefa98a155255019df7367b5f
Gustavo Foletto Mora <gusfmora@gmail.com>
2018-02-04 23:29:26 -0200

    Move clang format to config repo and reformat code

--- test/ut.h
+++ test/ut.h
@@ -10,41 +10,41 @@ struct ut {
 
 extern struct ut unit_test;
 
-#define ASSERT(test)                                                           \
-    do {                                                                       \
-        if (!(test)) {                                                         \
-            printf("%s:%d: assertion failed: %s\n", __FILE__, __LINE__,        \
-                   #test);                                                     \
-            return -1;                                                         \
-        }                                                                      \
+#define ASSERT(test)                                                    \
+    do {                                                                \
+        if (!(test)) {                                                  \
+            printf("%s:%d: assertion failed: %s\n", __FILE__, __LINE__, \
+                   #test);                                              \
+            return -1;                                                  \
+        }                                                               \
     } while (0)
 
-#define ASSERTD(test, val1, val2, fmt)                                         \
-    do {                                                                       \
-        printf("val1: " fmt "\n", val1);                                       \
-        printf("val2: " fmt "\n", val2);                                       \
-        ASSERT(test);                                                          \
+#define ASSERTD(test, val1, val2, fmt)   \
+    do {                                 \
+        printf("val1: " fmt "\n", val1); \
+        printf("val2: " fmt "\n", val2); \
+        ASSERT(test);                    \
     } while (0)
 
-#define ut_run(test)                                                           \
-    do {                                                                       \
-        unit_test.run++;                                                       \
-        int result = test();                                                   \
-        if (result != 0)                                                       \
-            unit_test.failed++;                                                \
-        printf("%s.%s: %s\n", __func__, #test, result ? "FAIL" : "OK");        \
+#define ut_run(test)                                                    \
+    do {                                                                \
+        unit_test.run++;                                                \
+        int result = test();                                            \
+        if (result != 0)                                                \
+            unit_test.failed++;                                         \
+        printf("%s.%s: %s\n", __func__, #test, result ? "FAIL" : "OK"); \
     } while (0)
 
-#define ut_result()                                                            \
-    do {                                                                       \
-        printf("%u test%s executed ", unit_test.run,                           \
-               unit_test.run > 1 ? "s" : "");                                  \
-        if (unit_test.failed == 0) {                                           \
-            printf("successfully\n");                                          \
-        } else {                                                               \
-            printf("and %u failed\n", unit_test.failed);                       \
-            return -1;                                                         \
-        }                                                                      \
+#define ut_result()                                      \
+    do {                                                 \
+        printf("%u test%s executed ", unit_test.run,     \
+               unit_test.run > 1 ? "s" : "");            \
+        if (unit_test.failed == 0) {                     \
+            printf("successfully\n");                    \
+        } else {                                         \
+            printf("and %u failed\n", unit_test.failed); \
+            return -1;                                   \
+        }                                                \
     } while (0)
 
 #endif /* GUSLIB_UT_H */
